2a6ec1d6ba845833e1e2244ae9379d2adc2a971b
CavalliB <Cerdopigman12@hotmail.com>
2025-05-23 15:41:04 -0300

    ejercicio 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
     setBad(bad + 1)
   }
 
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,6 +28,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good - bad) / (good+neutral+bad)}</p>
+      <p>positive {(good / (good+neutral+bad)) * 100}%</p>
 
     </div>
   )
