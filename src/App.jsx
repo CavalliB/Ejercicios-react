ebbfe227d0d044cdd83870c18111c96ce6f75b54
CavalliB <Cerdopigman12@hotmail.com>
2025-05-23 16:45:33 -0300

    ejercicio 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
 import { useState } from 'react'
+import Statistics from './Statistics'
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const goodclick = () => {
-    setGood(good + 1)
-  }
-  const neutralclick = () => {
-    setNeutral(neutral + 1)
-  }
-  const badclick = () => {
-    setBad(bad + 1)
-  }
-
-
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={goodclick}>good</button>
-      <button onClick={neutralclick}>neutral</button>
-      <button onClick={badclick}>neutral</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {(good - bad) / (good+neutral+bad)}</p>
-      <p>positive {(good / (good+neutral+bad)) * 100}%</p>
-
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
