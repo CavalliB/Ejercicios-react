2e10329a9c540dc4d9baa5fc9191366bae2988ce
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-05-27 23:43:00 +0000

    votos en objeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { useState } from 'react'
 import Statistics from './Statistics'
 
 const App = () => {
+  const [votes,setvotes]= useState({
+  good: 0,
+  neutral: 0,
+  bad:0
+  })
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+
 
 const Button = ({click,text}) => (<button onClick={click}>{text}</button>)
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button click={() => setGood(good + 1)} text="good" />
-      <Button click={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button click={() => setBad(bad + 1)} text="bad" />
+      <Button click={() => setvotes({...votes, good:votes.good +1})} text="good" />
+      <Button click={() => setvotes({...votes, neutral:votes.neutral +1})} text="neutral" />
+      <Button click={() => setvotes({...votes, bad:votes.bad +1})} text="bad" />
 
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics votes={votes} />
     </div>
   )
 }
--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,22 +1,22 @@
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({votes }) => {
     
-    const all = good + neutral + bad
+    const all = votes.good + votes.neutral + votes.bad
     
     if (all===0){
         return <p>No feedback given</p>
     }
 
-    const average = (good - bad) / all
-    const positive = (good / all) * 100
+    const average = (votes.good - votes.bad) / all
+    const positive = (votes.good / all) * 100
 
     const StatisticLine = ({ text, value }) => (<p>{text} {value}</p>)
 
 
     return (
     <div>
-        <StatisticLine text="good" value ={good} />
-        <StatisticLine text="neutral" value ={neutral} />
-        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="good" value ={votes.good} />
+        <StatisticLine text="neutral" value ={votes.neutral} />
+        <StatisticLine text="bad" value ={votes.bad} />
         <StatisticLine text="all" value ={all} />
         <StatisticLine text="average" value ={average} />
         <StatisticLine text="positive" value ={positive} />
