3df1af3c520ab5d4a49e8544e702fb99cf45bb3b
CavalliB <Cerdopigman12@hotmail.com>
2025-06-01 21:25:32 -0300

    ejercicio 1.14 anecdotes final

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -13,7 +13,9 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
   
-
+  const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(Array(anecdotes.length).fill(0))
+  
   const voted= () => {
     const copy = [...votes]
     copy[selected] +=1
@@ -25,20 +27,29 @@ const App = () => {
   }
   console.log(aleatorio)
 
-  const [selected, setSelected] = useState(0)
-const [votes, setVote] = useState(Array(anecdotes.length).fill(0))
   const handleClick= () => {
   return (aleatorio = GenerarAleatorio(0,8)) //nose por que el min es inclusive y el max no xd
   }
-console.log(votes)
+
+  const mostVoted=((Math.max(...votes)))
+  const mostVote = votes.indexOf(mostVoted)
+
+  console.log(votes)
   return (
     <div>
-      {anecdotes[selected]}
+      
       <div>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}
+        <div>
+          <button onClick={voted}>Vote</button>
+          <button onClick={() => setSelected(handleClick())}>next anecdote</button>
+        </div>
         
-        <button onClick={voted}>Vote</button>
-        <button onClick={() => setSelected(handleClick())}>next anecdote</button>
       </div>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVote]}
+      <p>has {mostVoted} votes</p>
     </div>
   )
 }
