58c0b88b7c05e0fce7a8a54981962c7c29b8c912
CavalliB <Cerdopigman12@hotmail.com>
2025-06-03 14:20:02 -0300

    anecdotes feature

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -31,10 +31,29 @@ const App = () => {
   return (aleatorio = GenerarAleatorio(0,8)) //nose por que el min es inclusive y el max no xd
   }
 
+
+
   const mostVoted=((Math.max(...votes)))
   const mostVote = votes.indexOf(mostVoted)
 
   console.log(votes)
+  console.log(mostVote)
+
+  function noVotes(){
+    if(mostVoted!==0){
+      return (
+      <>
+        <h1>Anecdote with most votes</h1>
+        {anecdotes[mostVote]}
+        <p>has {mostVoted} votes</p>
+      </>
+      )
+    }
+    else{
+//no mostrar
+    return null
+    }
+  }
   return (
     <div>
       
@@ -45,11 +64,9 @@ const App = () => {
           <button onClick={voted}>Vote</button>
           <button onClick={() => setSelected(handleClick())}>next anecdote</button>
         </div>
-        
+        {noVotes()}
       </div>
-      <h1>Anecdote with most votes</h1>
-      {anecdotes[mostVote]}
-      <p>has {mostVoted} votes</p>
+      
     </div>
   )
 }
