b3291b71d8e0d4a1db0f6b8e35c1986cd61fd527
CavalliB <Cerdopigman12@hotmail.com>
2025-06-06 11:33:19 -0300

    ejercicio 2.1 v2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,43 +1,50 @@
-const Course= () => {
+const Header = ({ course }) => <h1>{course}</h1>;
 
-
-const Header = ({ course }) => <h1>{course}</h1>
-
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
 
 const Content = ({ parts }) => (
   <div>
-
     <Part name={parts[0].name} exercises={parts[0].exercises} />
     <Part name={parts[1].name} exercises={parts[1].exercises} />
     <Part name={parts[2].name} exercises={parts[2].exercises} />
   </div>
-)
+);
 
-}
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
+
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
+  console.log(course);
+  return <Course course={course} />;
+};
 
-  return <Course course={course} />
-}
-
-export default App
+export default App;
