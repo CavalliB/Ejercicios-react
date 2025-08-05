a6dd56307a02b74acf7bc02e2b65b1b89e7b9229
CavalliB <Cerdopigman12@hotmail.com>
2025-08-05 17:36:08 -0300

    ejercicio 2.8

--- AgendaTelefonica/src/App.jsx
+++ AgendaTelefonica/src/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3001/persons").then((response) => {
@@ -15,6 +16,7 @@ const App = () => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     };
     if (persons.some((person) => person.name === newName)) {
@@ -26,13 +28,18 @@ const App = () => {
       .post("http://localhost:3001/persons", personObject)
       .then((response) => {
         setPersons(persons.concat(response.data));
-        setNewName("");
+        setNewName(""), setNewNumber("");
       });
   };
 
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -40,7 +47,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNoteChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,7 +58,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <li key={person.id}>{person.name}</li>
+        <li key={person.id}>{person.name}{" "}{person.number}</li>
       ))}
     </div>
   );
