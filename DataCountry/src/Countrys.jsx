3c1c5f045a266190a559d987a0d3a5aaadc212c4
CavalliB <Cerdopigman12@hotmail.com>
2025-08-25 22:50:55 -0300

    Ejercicio 2.19

--- DataCountry/src/Countrys.jsx
+++ DataCountry/src/Countrys.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const CountrysList = ({ countrys, newFilter }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
   const CountrysToShow = newFilter
     ? countrys.filter((Country) =>
         Country.name.common.toLowerCase().includes(newFilter.toLowerCase())
@@ -9,45 +12,47 @@ const CountrysList = ({ countrys, newFilter }) => {
     return "Too many matches, specify another filter";
   }
   if (CountrysToShow.length === 1) {
-    return (
-      <>
-        {CountrysToShow.map((country) => (
-          <div key={country.cca3}>
-            <h1>{country.name.common}</h1>
-            <div>
-              {"Capital "}
-              {country.capital}
-            </div>
-            <div>
-              {"Area "}
-              {country.area}
-            </div>
-            <h2>Languages</h2>
-            <ul>
-              {Object.values(country.languages).map((lang) => (
-                <li key={lang}>{lang}</li>
-              ))}
-            </ul>
-            <div>
-              <img
-                src={`https://flagcdn.com/w320/${country.cca2.toLowerCase()}.png`}
-                alt=""
-              />
-            </div>
-          </div>
-        ))}
-      </>
-    );
+    return <CountryDetail country={CountrysToShow[0]} />;
   }
 
   return (
     <div>
       <ul>
         {CountrysToShow.map((country) => (
-          <li key={country.cca3}>{country.name.common}</li>
+          <li key={country.cca3}>
+            {country.name.common}
+            <button onClick={() => setSelectedCountry(country)}>Show</button>
+          </li>
         ))}
       </ul>
+      {selectedCountry && <CountryDetail country={selectedCountry} />}
     </div>
   );
 };
+
+const CountryDetail = ({ country }) => (
+  <>
+    <h1>{country.name.common}</h1>
+    <div>
+      {"Capital "}
+      {country.capital}
+    </div>
+    <div>
+      {"Area "}
+      {country.area}
+    </div>
+    <h2>Languages</h2>
+    <ul>
+      {Object.values(country.languages).map((lang) => (
+        <li key={lang}>{lang}</li>
+      ))}
+    </ul>
+    <div>
+      <img
+        src={`https://flagcdn.com/w320/${country.cca2.toLowerCase()}.png`}
+        alt=""
+      />
+    </div>
+  </>
+);
 export default CountrysList;
